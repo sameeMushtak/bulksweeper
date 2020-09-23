50403b15705009137f25b2152c0f5a91a5f2c88f
sameeMushtak <samee.mushtak@gmail.com>
2020-09-22 23:13:37 -0400

    Made progress on command parsing with regex

--- bulksweeper.cpp
+++ bulksweeper.cpp
@@ -3,16 +3,23 @@
 #include <random>
 #include <chrono>
 #include <set>
+#include <string>
 
+std::vector<std::vector<int>> populate_board(std::vector<std::vector<int>> board, std::set<int> mine_locs, int height, int width);
 std::set<int> rand_combi(int n, int k, std::mt19937 &engine, std::uniform_real_distribution<double> distribution);
 void draw_board(std::vector<std::vector<int>> board, std::vector<std::vector<int>> cleared, int height, int width);
 bool inside_board(int row, int col, int height, int width);
 
 int main()
 {
+    unsigned seed = std::chrono::steady_clock::now().time_since_epoch().count();
+    std::mt19937 engine(seed);
+    std::uniform_real_distribution<double> distribution(0.0, 1.0);
+
     int height;
     int width;
     int num_mines;
+    bool board_cleared = false;
 
     // Get board parameters from user
     std::cout << "Board Height: ";
@@ -26,10 +33,34 @@ int main()
     // Initializing board
     // -1 => Mine
     std::vector<std::vector<int>> board = std::vector<std::vector<int>>(height, std::vector<int>(width, 0));
-    unsigned seed = std::chrono::steady_clock::now().time_since_epoch().count();
-    std::mt19937 engine(seed);
-    std::uniform_real_distribution<double> distribution(0.0, 1.0);
-    std::set<int> mine_locs = rand_combi(tiles, num_mines, engine, distribution);
+    // Matrix of cleared cells
+    // 0 => Uncleared
+    // 1 => Cleared in a try
+    // 2 => Confirmed clear (after a try)
+    std::vector <std::vector<int>> cleared = std::vector<std::vector<int>>(height, std::vector<int>(width, 0));
+    draw_board(board, cleared, height, width);
+
+    std::string cmd;
+    while (!board_cleared) {
+        std::cin >> cmd;
+        if (cmd == "yes") {
+            board_cleared = true;
+        }
+        draw_board(board, cleared, height, width);
+    }
+    // std::set<int> mine_locs = rand_combi(tiles, num_mines, engine, distribution);
+    // board = populate_board(board, mine_locs, height, width);
+
+}
+
+// Algorithm for sampling without replacement
+// https://stackoverflow.com/questions/311703/algorithm-for-sampling-without-replacement
+// Perhaps mines may be generated after first clear to ensure first clear is safe.
+// Generate num_mines mines on tiles-1 tiles. If a mine index is greater than or equal to the index of this first click, shift up by 1.
+
+std::vector<std::vector<int>> populate_board(std::vector<std::vector<int>> board, std::set<int> mine_locs, int height, int width)
+{
+    // std::set<int> mine_locs = rand_combi(tiles, num_mines, engine, distribution);
     for (auto loc : mine_locs) {
         board[loc / width][loc % width] = -1;
     }
@@ -52,19 +83,8 @@ int main()
             }
         }
     }
-
-    // Matrix of cleared cells
-    // 0 => Uncleared
-    // 1 => Cleared in a try
-    // 2 => Confirmed clear (after a try)
-    std::vector <std::vector <int>> cleared = std::vector<std::vector <int>>(height, std::vector<int>(width, 2));
-    draw_board(board, cleared, width, height);
+    return board;
 }
-
-// Algorithm for sampling without replacement
-// https://stackoverflow.com/questions/311703/algorithm-for-sampling-without-replacement
-// Perhaps mines may be generated after first clear to ensure first clear is safe.
-// Generate num_mines mines on tiles-1 tiles. If a mine index is greater than or equal to the index of this first click, shift up by 1.
 std::set<int> rand_combi(int n, int k, std::mt19937 &generator, std::uniform_real_distribution<double> distribution)
 {
     std::set<int> combi;
@@ -105,6 +125,7 @@ void draw_board(std::vector<std::vector<int>> board, std::vector<std::vector<int
     }
 }
 
+// Determines if a pair of indices corresponds to a position within the game board.
 bool inside_board(int row, int col, int height, int width) {
     return row >= 0 && col >= 0 && row < height && col < width;
 }
